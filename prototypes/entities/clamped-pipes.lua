005ee410170033cc1043067dac4307a3e32619af
TheStaplergun <staplergun@gmail.com>
2018-09-26 14:19:35 -0400

    Removed selection tool bloat

--- prototypes/entities/clamped-pipes.lua
+++ prototypes/entities/clamped-pipes.lua
@@ -106,24 +106,4 @@ for i, pipe in pairs(data.raw['pipe']) do
         end
     end
 end
-data:extend(pipeEntities,
-
-{
-    type = 'selection-tool',
-    name = 'picker-pipe-clamper',
-    icon = '__PickerPipeTools__/graphics/icons/hr-lock.png',
-    icon_size = 64,
-    flags = {'hidden'},
-    subgroup = 'tool',
-    order = 'c[selection-tool]-a[pipe-cleaner]',
-    stack_size = 1,
-    stackable = false,
-    selection_color = {r = 1, g = 0, b = 0},
-    alt_selection_color = {r = 0, g = 1, b = 0},
-    selection_mode = {'same-force', 'buildable-type', 'items-to-place'},
-    alt_selection_mode = {'same-force', 'buildable-type', 'items-to-place'},
-    selection_cursor_box_type = 'copy',
-    alt_selection_cursor_box_type = 'copy',
-    always_include_tiles = false,
-    show_in_library = true
-})
+data:extend(pipeEntities)
