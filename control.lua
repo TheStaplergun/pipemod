d9434f804049ad1345f78b885474c75e4b7d0d7c
kimberly wood <staplergun@gmail.com>
2018-11-22 23:32:11 -0500

    Remove deprecated code for cheatmode hax

--- control.lua
+++ control.lua
@@ -131,15 +131,6 @@ local function minus_valve(event)
 end
 Event.register('minus-valve', minus_valve)
 
--- Cheat Mode HAX
-local function on_player_pipette(event)
-    if event.used_cheat_mode and event.item and advancedPiping.correctBluePrintTable[event.item.name] then
-        local player = game.players[event.player_index]
-        player.cursor_stack.set_stack(advancedPiping.correctBluePrintTable[event.item.name])
-    end
-end
-Event.register(defines.events.on_player_pipette, on_player_pipette)
-
 local function get_pipe_table()
     return advancedPiping.pipetable
 end
