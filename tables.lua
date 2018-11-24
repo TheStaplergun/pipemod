23cb61831d9b56fc928b7e1697a35e688db50495
kimberly wood <staplergun@gmail.com>
2018-11-23 21:59:49 -0500

    Cut old code

--- tables.lua
+++ tables.lua
@@ -54,6 +54,60 @@ data.getRotatedPipe = {
     ['one-to-three-left-t3-pipe'] = 'one-to-three-forward-t3-pipe'
 }
 
+data.getReverseRotatedPipe = {
+    -- One to One pipes
+    ['one-to-one-forward-pipe'] = 'one-to-one-right-pipe',
+    ['one-to-one-right-pipe'] = 'one-to-one-reverse-pipe',
+    ['one-to-one-reverse-pipe'] = 'one-to-one-left-pipe',
+    ['one-to-one-left-pipe'] = 'one-to-one-forward-pipe',
+    -- One to Two pipes
+    ['one-to-two-parallel-pipe'] = 'one-to-two-perpendicular-pipe',
+    ['one-to-two-perpendicular-pipe'] = 'one-to-two-parallel-pipe',
+    ['one-to-two-L-FL-pipe'] = 'one-to-two-L-FR-pipe',
+    ['one-to-two-L-FR-pipe'] = 'one-to-two-L-RR-pipe',
+    ['one-to-two-L-RR-pipe'] = 'one-to-two-L-RL-pipe',
+    ['one-to-two-L-RL-pipe'] = 'one-to-two-L-FL-pipe',
+    -- One to Three pipes
+    ['one-to-three-forward-pipe'] = 'one-to-three-right-pipe',
+    ['one-to-three-right-pipe'] = 'one-to-three-reverse-pipe',
+    ['one-to-three-reverse-pipe'] = 'one-to-three-left-pipe',
+    ['one-to-three-left-pipe'] = 'one-to-three-forward-pipe',
+    -- One to One t2 pipes
+    ['one-to-one-forward-t2-pipe'] = 'one-to-one-right-t2-pipe',
+    ['one-to-one-right-t2-pipe'] = 'one-to-one-reverse-t2-pipe',
+    ['one-to-one-reverse-t2-pipe'] = 'one-to-one-left-t2-pipe',
+    ['one-to-one-left-t2-pipe'] = 'one-to-one-forward-t2-pipe',
+    -- One to Two t2 pipes
+    ['one-to-two-parallel-t2-pipe'] = 'one-to-two-perpendicular-t2-pipe',
+    ['one-to-two-perpendicular-t2-pipe'] = 'one-to-two-parallel-t2-pipe',
+    ['one-to-two-L-FL-t2-pipe'] = 'one-to-two-L-FR-t2-pipe',
+    ['one-to-two-L-FR-t2-pipe'] = 'one-to-two-L-RR-t2-pipe',
+    ['one-to-two-L-RR-t2-pipe'] = 'one-to-two-L-RL-t2-pipe',
+    ['one-to-two-L-RL-t2-pipe'] = 'one-to-two-L-FL-t2-pipe',
+    -- One to Three t2 pipes
+    ['one-to-three-forward-t2-pipe'] = 'one-to-three-right-t2-pipe',
+    ['one-to-three-right-t2-pipe'] = 'one-to-three-reverse-t2-pipe',
+    ['one-to-three-reverse-t2-pipe'] = 'one-to-three-left-t2-pipe',
+    ['one-to-three-left-t2-pipe'] = 'one-to-three-forward-t2-pipe',
+    -- One to One t3 pipes
+    ['one-to-one-forward-t3-pipe'] = 'one-to-one-right-t3-pipe',
+    ['one-to-one-right-t3-pipe'] = 'one-to-one-reverse-t3-pipe',
+    ['one-to-one-reverse-t3-pipe'] = 'one-to-one-left-t3-pipe',
+    ['one-to-one-left-t3-pipe'] = 'one-to-one-forward-t3-pipe',
+    -- One to Two t3 pipes
+    ['one-to-two-parallel-t3-pipe'] = 'one-to-two-perpendicular-t3-pipe',
+    ['one-to-two-perpendicular-t3-pipe'] = 'one-to-two-parallel-t3-pipe',
+    ['one-to-two-L-FL-t3-pipe'] = 'one-to-two-L-FR-t3-pipe',
+    ['one-to-two-L-FR-t3-pipe'] = 'one-to-two-L-RR-t3-pipe',
+    ['one-to-two-L-RR-t3-pipe'] = 'one-to-two-L-RL-t3-pipe',
+    ['one-to-two-L-RL-t3-pipe'] = 'one-to-two-L-FL-t3-pipe',
+    -- One to Three t3 pipes
+    ['one-to-three-forward-t3-pipe'] = 'one-to-three-right-t3-pipe',
+    ['one-to-three-right-t3-pipe'] = 'one-to-three-reverse-t3-pipe',
+    ['one-to-three-reverse-t3-pipe'] = 'one-to-three-left-t3-pipe',
+    ['one-to-three-left-t3-pipe'] = 'one-to-three-forward-t3-pipe'
+}
+
 data.pipetable = {
     ['one-to-two-parallel-pipe'] = 3,
     ['one-to-two-perpendicular-pipe'] = 3,
