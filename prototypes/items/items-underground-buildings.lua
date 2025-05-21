8601000eca9c776f08278ca7895ce05aa64f0289
TheStaplergun <staplergun@gmail.com>
2025-05-21 15:49:58 -0400

    Add color to pump icons to distinguish tiers

--- prototypes/items/items-underground-buildings.lua
+++ prototypes/items/items-underground-buildings.lua
@@ -1,11 +1,29 @@
+local tints = {
+    blue = {r=38/255,g=173/255,b=227/255,a=1},
+    red = {r=227/255,g=38/255,b=45/255,a=1},
+    yellow = {r=255/255,g=191/255,b=0/255,a=1}
+}
+
 data:extend(
   {
     -- UNDERGROUND BUILDINGS
     {
       type = "item",
       name = "underground-mini-pump",
-      icon = "__underground-pipe-pack__/graphics/icons/underground-mini-pump.png",
-      icon_size = 32,
+      icons = {
+        {
+          icon = "__underground-pipe-pack__/graphics/icons/underground-mini-pump.png",
+          icon_size = 32,
+        },
+        {
+          icon = "__underground-pipe-pack__/graphics/icons/hr-underground-metal-mask.png",
+          icon_size = 96,
+          scale = 1/2,
+          tint = tints.yellow,
+          floating = true
+        },
+      },
+      --icon_size = 32,
       flags = nil,
       subgroup = "underground-buildings",
       order = "a-a",
@@ -15,8 +33,20 @@ data:extend(
     {
       type = "item",
       name = "underground-mini-pump-t2",
-      icon = "__underground-pipe-pack__/graphics/icons/underground-mini-pump.png",
-      icon_size = 32,
+      icons = {
+        {
+          icon = "__underground-pipe-pack__/graphics/icons/underground-mini-pump.png",
+          icon_size = 32,
+        },
+        {
+          icon = "__underground-pipe-pack__/graphics/icons/hr-underground-metal-mask.png",
+          icon_size = 96,
+          scale = 1/2,
+          tint = tints.red,
+          floating = true
+        },
+      },
+      --icon_size = 32,
       flags = nil,
       subgroup = "underground-buildings",
       order = "a-b",
@@ -26,8 +56,20 @@ data:extend(
     {
       type = "item",
       name = "underground-mini-pump-t3",
-      icon = "__underground-pipe-pack__/graphics/icons/underground-mini-pump.png",
-      icon_size = 32,
+      icons = {
+        {
+          icon = "__underground-pipe-pack__/graphics/icons/underground-mini-pump.png",
+          icon_size = 32,
+        },
+        {
+          icon = "__underground-pipe-pack__/graphics/icons/hr-underground-metal-mask.png",
+          icon_size = 96,
+          scale = 1/2,
+          tint = tints.blue,
+          floating = true
+        },
+      },
+      --icon_size = 32,
       flags = nil,
       subgroup = "underground-buildings",
       order = "a-c",
